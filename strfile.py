3921a89363d48cadb800b6d977297c885f9277e9
NormalExisting <WILLIAMSTORAGE69@gmail.com>
2025-02-07 23:36:23 -0600

    upgrade strfile.py

--- strfile.py
+++ strfile.py
@@ -6,18 +6,29 @@ import argparse
 This program, given a file, generates a C++ function to return the contents of the file as a string.
 The function is intended to belong in its own header file and has an #include <string> at the beginning.
 
+Although this works on any file it is most suited for .obj (wavefront) files and has special flags for those.
+Use them at your own risk!
+
 '''
 
 '''
 input is the input file
 name is the name of the string we save to
 out is the output file
+
+ignore-t = ignore texture data
+ignore-n = ignore normal data
+ignore-useless = ignore comments and blank lines
 '''
 
 # DEFAULT ARG VALUES
 input = "misc/mcrproto.obj"
 name = "protostring"
 out = "misc/mcrproto.dat"
+
+ignore_t = False
+ignore_n = False
+ignore_useless = False
 
 sn = "res"
 
@@ -29,6 +40,12 @@ def output():
 	string = string + "\tstd::string " + sn + " = \"\";\n"
 
 	for line in FILE:
+		if ignore_t and (len(line) >= 2 and line[0:2] == "vt"):
+			continue
+		if ignore_n and (len(line) >= 2 and line[0:2] == "vn"):
+			continue
+		if ignore_useless and (len(line.strip()) <= 0 or line[0] == '#'):
+			continue
 		string = string + "\t" + sn + " = " + sn + "+ \"" + line.strip() + "\\n\";\n"
 	
 	string = string + "\treturn " + sn + ";\n}"
@@ -44,6 +61,7 @@ def output():
 
 -ignore-t = ignore textures
 -ignore-n = ignore normals
+-ignore-useless = ignore blank lines/comments
 
 '''
 
@@ -57,6 +75,9 @@ if __name__ == "__main__":
 	parser.add_argument('-input', default = input)
 	parser.add_argument('--name', default = name)
 	parser.add_argument('-out', default = out)
+	parser.add_argument("-ignore_t", action = "store_true")
+	parser.add_argument("-ignore_n", action = "store_true")
+	parser.add_argument("-ignore_useless", action = "store_true")
 
 	args = parser.parse_args()
 
@@ -64,4 +85,7 @@ if __name__ == "__main__":
 	name = args.name
 	out = args.out
 
+	ignore_t = args.ignore_t
+	ignore_n = args.ignore_n
+	ignore_useless = args.ignore_useless
 	output()
